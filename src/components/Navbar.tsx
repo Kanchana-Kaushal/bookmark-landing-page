37c4dc7f467de91a8941335bcadba1e76c7d8b36
Kanchana K. <kanchanak.bandara@gmail.com>
2025-06-14 17:16:01 +0530

    Finished creating base version of navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import Logo from "../assets/logo-bookmark.svg?react";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="font-rubik mx-auto flex w-8/10 items-center justify-between py-8">
+        <Logo className="fill-current text-red-500" />
+
+        <div className="flex items-center gap-8">
+          <img src="/public/icon-hamburger.svg" className="md:hidden" />
+
+          <ul className="text-custom-Blue-950 hidden gap-8 uppercase md:flex">
+            <li className="cursor-pointer">features</li>
+            <li className="cursor-pointer">pricing</li>
+            <li className="cursor-pointer">contact</li>
+          </ul>
+
+          <button className="bg-custom-Red-400 text-custom-Grey-50 hidden cursor-pointer rounded-sm px-6 py-2 uppercase drop-shadow-lg md:flex">
+            login
+          </button>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
